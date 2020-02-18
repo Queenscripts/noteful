14d3d65fb54d75c1585724d9c910ab0c7db022ad
Queen Shabazz <queennefertitishabazz@gmail.com>
2020-02-18 13:16:20 -0800

    procfile cleanup

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
     PORT: process.env.PORT || 8000,
     NODE_ENV: process.env.NODE_ENV || 'development',
-    DB_URL: process.env.DB_URL || 'postgresql://dunder-mifflin@localhost/noteful',
-    TEST_DB_URL: process.env.TEST_DB_URL || 'postgresql://dunder-mifflin@localhost/noteful-test'
+    DB_URL: process.env.DB_URL || 'postgresql://dunder@localhost/noteful',
+    TEST_DB_URL: process.env.TEST_DB_URL || 'postgresql://dunder@localhost/noteful-test'
 }
