d98e1cf16a1c7fd865bb9ee2de8260b1eaf5843a
Queen Shabazz <queennefertitishabazz@gmail.com>
2020-02-18 16:44:41 -0800

    reworked config

--- postgrator-production-config.js
+++ postgrator-production-config.js
@@ -1,12 +1,25 @@
+// require('dotenv').config();
+
+// module.exports = {
+//   "migrationDirectory": "migrations",
+//   "driver": "pg",
+//   "host": process.env.PROD_MIGRATION_DB_HOST,
+//   "port": process.env.PROD_MIGRATION_DB_PORT,
+//   "database": process.env.PROD_MIGRATION_DB_NAME,
+//   "username": process.env.PROD_MIGRATION_DB_USER,
+//   "password": process.env.PROD_MIGRATION_DB_PASS,
+//   "ssl": true
+// }
+
+
 require('dotenv').config();
 
 module.exports = {
-  "migrationDirectory": "migrations",
+  "migrationsDirectory": "migrations",
   "driver": "pg",
-  "host": process.env.PROD_MIGRATION_DB_HOST,
-  "port": process.env.PROD_MIGRATION_DB_PORT,
-  "database": process.env.PROD_MIGRATION_DB_NAME,
-  "username": process.env.PROD_MIGRATION_DB_USER,
-  "password": process.env.PROD_MIGRATION_DB_PASS,
-  "ssl": true
+  "connectionString": (process.env.NODE_ENV === 'test')
+    ? process.env.TEST_DATABASE_URL
+    : process.env.DATABASE_URL,
+  "ssl": !!process.env.SSL,
+  "validateChecksums": false, // Set to false to skip validation,
 }
